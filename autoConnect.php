d3dd6b631a0ea177ba89c229292ca936f1c005eb
Adrien <adrien.buot@edu.ece.fr>
2019-04-29 17:12:24 +0200

    debug name

--- autoConnect.php
+++ autoConnect.php
@@ -1,5 +1,5 @@
 <?php
-	include("openDB.php");
+	include("sendRequest.php");
 	if(!isset($_SESSION['ID_people']) && isset($_COOKIE['pseudo']) && $COOKIE['password']){
 		sendRequest("SELECT Id FROM People WHERE Pseudo ='".$_COOKIE['pseudo'] . "' AND Mot_De_Passe ='" .$_COOKIE['password'] ."'");
 		while($data = mysqli_fetch_assoc($result)){
