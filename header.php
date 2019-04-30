e1ae3035d9a02dd396a58c8fe378f34c1a248f1c
Adrien <adrien.buot@edu.ece.fr>
2019-04-30 15:38:41 +0200

    gestion of the top bar, depending on the type of user

--- header.php
+++ header.php
@@ -19,11 +19,35 @@
 	    </li>
 
 	    <li class="nav-item">
-	      <a class="nav-link" href="connexionPage.php"><?php echo isset($_SESSION['ID_people'])? "Mon Profil":"Se connecter";?></a>
+	    	<?php
+					if(isset($_SESSION['ID_people'])){
+						echo '<a class="nav-link" href="monCompte.php">Mon Profil</a>';
+					}else{
+						echo '<a class="nav-link" href="connexionPage.php">Se connecter</a>';
+					}
+	    	?>
 	    </li>
+	    <?php 
+	    	if(isset($_SESSION['ID_people']) && $_SESSION['type_utilisateur'] == 2){
+	    ?>
 	    <li class="nav-item">
 	      <a class="nav-link" href="#">Mon Panier</a>
 	    </li>
+			<?php
+				}else if(isset($_SESSION['ID_people']) && $_SESSION['type_utilisateur'] == 1){
+			?>
+			<li class="nav-item">
+	      <a class="nav-link" href="newItem.php">Ajouter article</a>
+	    </li>
+	    <?php
+	    	}else if(isset($_SESSION['ID_people'])){
+	    ?>
+	    <li class="nav-item">
+	      <a class="nav-link" href="#">Gérer vendeurs</a>
+	    </li>
+	    <?php
+	    	}
+	    ?>
 	  </ul>
 	  <!-- Barre de recherche -->
 	  <form class="navbar-form navbar-right form-inline" action="" method="post">
