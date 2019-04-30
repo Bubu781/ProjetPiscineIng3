fb6eeb5a7f75e7fc9d986ee1bd6f7b023030eb36
Adrien <adrien.buot@edu.ece.fr>
2019-04-30 12:29:26 +0200

    debug autoConnect

--- autoConnect.php
+++ autoConnect.php
@@ -1,6 +1,6 @@
 <?php
 	include("sendRequest.php");
-	if(!isset($_SESSION['ID_people']) && isset($_COOKIE['pseudo']) && $COOKIE['password']){
+	if(!isset($_SESSION['ID_people']) && isset($_COOKIE['pseudo']) && isset($_COOKIE['password'])){
 		sendRequest("SELECT Id FROM People WHERE Pseudo ='".$_COOKIE['pseudo'] . "' AND Mot_De_Passe ='" .$_COOKIE['password'] ."'");
 		while($data = mysqli_fetch_assoc($result)){
 			$_SESSION['ID_people'] = $data['Id'];
