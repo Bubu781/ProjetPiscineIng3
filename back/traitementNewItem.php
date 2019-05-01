b7ef0a8e6f7971eef1e22f5659ff90584cab643a
leo <leoguich@yahoo.fr>
2019-05-01 18:42:37 +0200

    begening of new item

---
+++ back/traitementNewItem.php
@@ -0,0 +1,129 @@
+<?php
+	session_start();
+
+	include("../sendRequest.php");
+	include("../autoConnect.php");
+
+	$_SESSION['type_utilisateur'] = 1;
+	$_SESSION['ID_people'] = 3;
+?>
+
+<?php
+	
+	$nom = isset($_POST["nom"])? $_POST["nom"] : ""; //if-then-else
+	$Marque = isset($_POST["Marque"])? $_POST["Marque"] : ""; //if-then-else
+	$description = isset($_POST["description"])? $_POST["description"] : ""; //if-then-else
+	$prix = isset($_POST["prix"])? $_POST["prix"] : ""; //if-then-else
+	$categorie = isset($_POST["categorie"])? $_POST["categorie"] : ""; //if-then-else
+
+	if (isset($_FILES['photo1']) AND $_FILES['photo1']['error'] == 0){
+    // Testons si le fichier n'est pas trop gros
+    if ($_FILES['photo1']['size'] <= 3145728){
+      // Testons si l'extension est autorisée
+      $infosfichier = pathinfo($_FILES['photo1']['name']);
+      $extension_upload = $infosfichier['extension'];
+      $extensions_autorisees = array('jpg', 'jpeg', 'gif', 'png');
+      if (in_array($extension_upload, $extensions_autorisees)){
+              move_uploaded_file($_FILES['photo1']['tmp_name'], '../front/Media/' . basename($_FILES['photo1']['name']));}
+      else{echo 'extention non-autorisee';}}
+    else{echo 'image1 trop grosse';}
+  }
+	else {echo'Pb image 1';}
+
+
+	if (isset($_FILES['photo2']) AND $_FILES['photo2']['error'] == 0){
+    // Testons si le fichier n'est pas trop gros
+    if ($_FILES['photo2']['size'] <= 3145728){
+      // Testons si l'extension est autorisée
+      $infosfichier = pathinfo($_FILES['photo2']['name']);
+      $extension_upload = $infosfichier['extension'];
+      $extensions_autorisees = array('jpg', 'jpeg', 'gif', 'png');
+      if (in_array($extension_upload, $extensions_autorisees)){
+              move_uploaded_file($_FILES['photo2']['tmp_name'], '../front/Media/' . basename($_FILES['photo2']['name']));}
+      else{echo 'extention non-autorisee';}}
+    else{echo 'image1 trop grosse';}
+  }
+	else {echo'Pb image 1';}
+
+
+
+	if (isset($_FILES['photo3']) AND $_FILES['photo3']['error'] == 0){
+    // Testons si le fichier n'est pas trop gros
+    if ($_FILES['photo3']['size'] <= 3145728){
+      // Testons si l'extension est autorisée
+      $infosfichier = pathinfo($_FILES['photo3']['name']);
+      $extension_upload = $infosfichier['extension'];
+      $extensions_autorisees = array('jpg', 'jpeg', 'gif', 'png');
+      if (in_array($extension_upload, $extensions_autorisees)){
+              move_uploaded_file($_FILES['photo3']['tmp_name'], '../front/Media/' . basename($_FILES['photo3']['name']));}
+      else{echo 'extention non-autorisee';}}
+    else{echo 'image1 trop grosse';}
+  }
+	else {echo'Pb image 1';}
+
+
+
+	if (isset($_FILES['photo4']) AND $_FILES['photo4']['error'] == 0){
+    // Testons si le fichier n'est pas trop gros
+    if ($_FILES['photo4']['size'] <= 3145728){
+      // Testons si l'extension est autorisée
+      $infosfichier = pathinfo($_FILES['photo4']['name']);
+      $extension_upload = $infosfichier['extension'];
+      $extensions_autorisees = array('jpg', 'jpeg', 'gif', 'png');
+      if (in_array($extension_upload, $extensions_autorisees)){
+              move_uploaded_file($_FILES['photo4']['tmp_name'], '../front/Media/' . basename($_FILES['photo4']['name']));}
+      else{echo 'extention non-autorisee';}}
+    else{echo 'image1 trop grosse';}
+  }
+	else {echo'Pb image 1';}
+
+
+	if (isset($_FILES['photo5']) AND $_FILES['photo5']['error'] == 0){
+    // Testons si le fichier n'est pas trop gros
+    if ($_FILES['photo5']['size'] <= 3145728){
+      // Testons si l'extension est autorisée
+      $infosfichier = pathinfo($_FILES['photo5']['name']);
+      $extension_upload = $infosfichier['extension'];
+      $extensions_autorisees = array('jpg', 'jpeg', 'gif', 'png');
+      if (in_array($extension_upload, $extensions_autorisees)){
+              move_uploaded_file($_FILES['photo5']['tmp_name'], '../front/Media/' . basename($_FILES['photo5']['name']));}
+      else{echo 'extention non-autorisee';}}
+    else{echo 'image1 trop grosse';}
+  }
+	else {echo'Pb image 1';}
+
+	// media
+	sendRequest('INSERT INTO Media(Path1,Path2,Path3,Path4,Path5) VALUES( "Media/'.$_FILES['photo1']['name'].'");');
+	$media = sendRequest('SELECT MAX(Id) FROM media');
+	$data = mysqli_fetch_assoc($media);
+
+
+	//echo 'INSERT INTO People(Nom, Prenom, Pseudo, Mail, N_Telephonne, Mot_De_Passe,media) VALUES("'.$nom.'","'.$prenom.'","'.$pseudo.'","'.$mail.'","'.$tel.'","'.$mdp1.'",'.$data['MAX(Id)'].');';
+	sendRequest('INSERT INTO People(Nom, Prenom, Pseudo, Mail, N_Telephonne, Mot_De_Passe,media) VALUES("'.$nom.'","'.$prenom.'","'.$pseudo.'","'.$mail.'","'.$tel.'","'.$mdp1.'",'.$data['MAX(Id)'].');');
+	$media = sendRequest('SELECT MAX(Id) FROM People');
+	$data = mysqli_fetch_assoc($media);
+
+	if ($categorie == 'vendeur'){
+		$banque = isset($_POST["banque"])? $_POST["banque"] : ""; //if-then-else
+
+		sendRequest('INSERT INTO Vendeur(Porte_Monnaie,people) VALUES( '.$banque.', '.$data['MAX(Id)'].');');
+	}
+	else if ($categorie == 'client'){
+
+		$banque = isset($_POST["banque"])? $_POST["banque"] : ""; //if-then-else
+		$pays = isset($_POST["pays"])? $_POST["pays"] : ""; //if-then-else
+		$code_postal = isset($_POST["code_postal"])? $_POST["code_postal"] : ""; //if-then-else
+		$ville = isset($_POST["ville"])? $_POST["ville"] : ""; //if-then-else
+		$adresse_l1 = isset($_POST["adresse_l1"])? $_POST["adresse_l1"] : ""; //if-then-else
+		$adresse_l2 = isset($_POST["adresse_l2"])? $_POST["adresse_l2"] : ""; //if-then-else
+		$type_carte = isset($_POST["type_carte"])? $_POST["type_carte"] : ""; //if-then-else
+		$Date_Expiration_Carte = isset($_POST["Date_Expiration_Carte"])? $_POST["Date_Expiration_Carte"] : ""; //if-then-else
+		$code_carte = isset($_POST["code_carte"])? $_POST["code_carte"] : ""; //if-then-else
+		$nom_Carte = isset($_POST["nom_Carte"])? $_POST["nom_Carte"] : ""; //if-then-else
+		$Num_Carte = isset($_POST["Num_Carte"])? $_POST["Num_Carte"] : ""; //if-then-else
+
+		sendRequest('	INSERT INTO Client(Porte_Monnaie, Code_Carte, Date_Expiration_Carte, Nom_Carte, Num_Carte, Type_Carte, Adresse_L1, Adresse_L2, Ville, Code_Postal, Pays, people) VALUES( '.$banque.', "'.$code_carte.'", "'.$Date_Expiration_Carte.'", "'.$nom_Carte.'","'.$Num_Carte.'","'.$type_carte.'", "'.$adresse_l1.'", "'.$adresse_l2.'", "'.$ville.'", '.$code_postal.', "'.$pays.'", '.$data['MAX(Id)'].');');
+
+	}
+
+?>
--- front/newItem.php
+++ front/newItem.php
@@ -30,29 +30,54 @@
 
 			<tr class="form-group">
 				<td>Nom :</td>
-				<td><input type="text" id="nom" class="form-control" placeholder="Saisisez le nom"></td>
+				<td><input type="text" id="nom" name="nom" class="form-control" placeholder="Saisisez le nom"></td>
 			</tr>
 
 			<tr class="form-group">
 				<td>Marque :</td>
-				<td><input type="text" id="Marque" class="form-control" placeholder="Saisisez le nom"></td>
+				<td><input type="text" id="Marque" name="Marque" class="form-control" placeholder="Saisisez le nom"></td>
 			</tr>
 
 			<tr class="form-group">
 				<td>Descrition :</td>
-				<td><textarea type="text" id="description" class="form-control" placeholder="Saisisez la description ici"></textarea> </td>
+				<td><textarea type="text" id="description" name="description" class="form-control" placeholder="Saisisez la description ici"></textarea> </td>
 			</tr>
 
 			<tr class="form-group">
 				<td>Prix :</td>
-				<td><input type="number" step="0.01" id="prix" class="form-control" placeholder="Saisisez le prix"></td>
+				<td><input type="number" step="0.01" id="prix" name="prix" class="form-control" placeholder="Saisisez le prix"></td>
+			</tr>
+
+			<tr class="form-group">
+				<td>Votre image n°1 :</td>
+				<td><input type="file" name="photo1" id="photo1" placeholder="votre image "required></td>
+			</tr>
+
+			<tr class="form-group">
+				<td>Votre image n°2 :</td>
+				<td><input type="file" name="photo2" id="photo2" placeholder="votre image "></td>
+			</tr>
+
+			<tr class="form-group">
+				<td>Votre image n°3 :</td>
+				<td><input type="file" name="photo3" id="photo3" placeholder="votre image "></td>
+			</tr>
+
+			<tr class="form-group">
+				<td>Votre image n°4 :</td>
+				<td><input type="file" name="photo4" id="photo4" placeholder="votre image "></td>
+			</tr>
+
+			<tr class="form-group">
+				<td>Votre image n°5 :</td>
+				<td><input type="file" name="photo5" id="photo5" placeholder="votre image "></td>
 			</tr>
 
 			<tr class="form-group">
 				<td>Catégorie :</td>
 				<td>
 					
-					<SELECT id="categorie" class="form-control" onclick="loadFormulaireNewItem()">
+					<SELECT id="categorie" name="categorie" class="form-control" onclick="loadFormulaireNewItem()">
 					<OPTION VALUE="Vetements" selected="selected">Vetements</OPTION>
 					<OPTION VALUE="Livres" >Livres</OPTION>
 					<OPTION VALUE="Musiques" >Musiques</OPTION>
@@ -69,23 +94,23 @@
 		<table>
 				<tr class="form-group">
 					<td>Couleur :</td>
-					<td><input type="text" id="Couleur" class="form-control" placeholder="Saisisez la couleur"></td>
+					<td><input type="text" id="Couleur"  name="Couleur" class="form-control" placeholder="Saisisez la couleur"></td>
 				</tr>
 
 				<tr class="form-group">
 					<td>Matière :</td>
-					<td><input type="text" id="matiere" class="form-control" placeholder="Saisisez la matière. ex : lin"></td>
+					<td><input type="text" id="matiere" name="matiere" class="form-control" placeholder="Saisisez la matière. ex : lin"></td>
 				</tr>
 
 				<tr class="form-group">
 					<td>type :</td>
-					<td><input type="text" id="type" class="form-control" placeholder="Saisisez le type. ex : teeshirt"></td>
+					<td><input type="text" id="type" name="type" class="form-control" placeholder="Saisisez le type. ex : teeshirt"></td>
 				</tr>
 
 				<tr class="form-group">
 					<td>Genre :</td>
 					<td>
-						<SELECT id="genre" class="form-control">
+						<SELECT id="genre" name="genre" class="form-control">
 						<OPTION VALUE="homme" selected="selected" >Homme</OPTION>
 						<OPTION VALUE="femme">Femme</OPTION>
 						</SELECT>
@@ -95,7 +120,7 @@
 				<tr class="form-group">
 					<td>Taille :</td>
 					<td>
-						<SELECT id="taille" class="form-control">
+						<SELECT id="taille" name="taille" class="form-control">
 						<OPTION VALUE="XS" >XS</OPTION>
 						<OPTION VALUE="S" >S</OPTION>
 						<OPTION VALUE="M" selected="selected" >M</OPTION>
